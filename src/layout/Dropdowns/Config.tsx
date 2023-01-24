63f776cddcb63ee8e8614e011a9fd420b569ad1e
malks14 <73920001+malks14@users.noreply.github.com>
2023-01-24 09:09:50 -0300

    feat: delete user logic

--- src/layout/Dropdowns/Config.tsx
+++ src/layout/Dropdowns/Config.tsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 import ConfirmDialog from '../../components/ConfirmDialog';
 import NewChatModal from '../../components/HomeChat/NewChatModal';
+import { NotificationFailure, NotificationSuccess } from '../../components/Notifications';
+import { useAppDispatch } from '../../redux/hooks';
+import { setLogoutData } from '../../redux/userSlice';
 import { DropDownProps } from '../../types/chat';
+import { useRouter } from "next/router";
+import apiClient from '../../utils/client';
+import { LoadRemove, LoadStart } from '../../components/Loading';
 
 function ConfigDropdown(dropDownProps: DropDownProps) {
   const { getChatsData, userData, isOpen } = dropDownProps;
 
   const [delDialogIsOpen, setDelDialogIsOpen] = useState(false);
   const [newChatModalIsOpen, setNewChatModalIsOpen] = useState(false);
+
+  const dispatch = useAppDispatch();
+
+  const router = useRouter()
 
   const handleDeleteUser = () => {
     setDelDialogIsOpen(true);
@@ -18,11 +28,24 @@ function ConfigDropdown(dropDownProps: DropDownProps) {
   };
 
   const handleConfirmDelete = () => {
+    LoadStart()
     /* 
       TODO: 
       1. Get current user data 
       2. Delete user 
     */
+   try {
+    // el usuario puede ser eliminado directamente, ya que conserva el token en el header
+    // ? revisar la API de users delete para ver ver como se elimina un usuario
+    const deleteUser = apiClient.delete("/users")
+    console.log(deleteUser);
+    NotificationSuccess("Usuario eliminado con exito")
+    dispatch(setLogoutData());
+    router.push("/")
+   } catch (error) {
+    NotificationFailure("No se pudo eliminar el usuario. Intentar mas tarde")
+   }
+   LoadRemove();
   };
 
   return (
