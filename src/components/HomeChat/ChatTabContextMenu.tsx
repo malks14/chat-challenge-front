0b6bb5c0a8fecb6db950d93b55e4f235d833f6b8
malks14 <73920001+malks14@users.noreply.github.com>
2023-01-24 12:02:33 -0300

    feat: applied new logic forwardRef for Radix child

--- src/components/HomeChat/ChatTabContextMenu.tsx
+++ src/components/HomeChat/ChatTabContextMenu.tsx
@@ -2,6 +2,7 @@ import * as Menu from '@radix-ui/react-context-menu';
 import styled from 'styled-components';
 import { useAppDispatch } from '../../redux/hooks';
 import { setIsOpenModal, setTicketStatus } from '../../redux/chatsSlice';
+import { forwardRef } from 'react';
 
 const Container = styled.div`
   background-color: #fff;
@@ -20,28 +21,33 @@ const Item = styled.div`
     background-color: #36dd81;
   }
 `;
-export default function ChatTabContextMenu() {
-  const dispatch = useAppDispatch()
 
-  const handleShowOpenTicket = () => {
-    // TODO: Show open ticket component.
-    dispatch(setIsOpenModal(true));
-    
-    dispatch(setTicketStatus(0))
-    
-  };
+const ChatTabContextMenu = forwardRef<HTMLDivElement>((props, ref) => {
+    const dispatch = useAppDispatch()
+  
+    const handleShowOpenTicket = () => {
+      // TODO: Show open ticket component.
+      dispatch(setIsOpenModal(true));
+      
+      dispatch(setTicketStatus(0))
+      
+    };
+  
+    const handleShowClosedTicket = () => {
+      // TODO: Show closed ticket component.
+      dispatch(setTicketStatus(1))
+      dispatch(setIsOpenModal(true))
+    };
+  
+    return (
+        <Container>
+          <Item ref={ref} {...props} onClick={handleShowOpenTicket}>Ver ticket abierto</Item>
+          <Menu.Separator />
+          <Item ref={ref} {...props} onClick={handleShowClosedTicket}>Ver ticket cerrado</Item>
+        </Container>
+    );
+  
+})
 
-  const handleShowClosedTicket = () => {
-    // TODO: Show closed ticket component.
-    dispatch(setTicketStatus(1))
-    dispatch(setIsOpenModal(true))
-  };
 
-  return (
-      <Container>
-        <Item onClick={handleShowOpenTicket}>Ver ticket abierto</Item>
-        <Menu.Separator />
-        <Item onClick={handleShowClosedTicket}>Ver ticket cerrado</Item>
-      </Container>
-  );
-}
+export default ChatTabContextMenu;
